7e4c96399779a5402050a59435bed3417e6d94ee
SkyyySi <53921371+SkyyySi@users.noreply.github.com>
2021-10-01 10:52:52 +0200

    Improved style pt. 1

--- LinuxHostsGenerator.sh
+++ LinuxHostsGenerator.sh
@@ -6,26 +6,28 @@
 
 rm -rf hosts-files
 mkdir hosts-files
-rm -f hosts.tmp
-rm -f hosts
-if [ -f "hosts-sources.txt" ]; then
-echo "-----Hosts files are downloading..."
-sleep 1s
-wget --no-check-certificate -i hosts-sources.txt -P hosts-files
-else 
-echo "hosts-sources.txt doesn't exist."
-exit
+rm -f hosts hosts.tmp
+
+if [[ ! -f "hosts-sources.txt" ]]; then
+    echo "hosts-sources.txt doesn't exist."
+    exit
 fi
+
+echo "----- Hosts files are downloading..."
+wget --no-check-certificate -i hosts-sources.txt -P hosts-files
+
 echo "-----All hosts files have been downloaded."
-sleep 1s
 echo "-----Merging all hosts files..."
-sleep 1s
-for f in hosts-files/*; do (cat "${f}"; echo) >> hosts.tmp; done
+
+for f in hosts-files/*; do
+    (cat "${f}"; echo) >> hosts.tmp
+done
+
 rm -rf hosts-files
+
 echo "-----All hosts files have been merged."
-sleep 1s
 echo "-----Compressing hosts file..."
-sleep 1s
+
 sed -i 's/#.*$//;/^$/d' hosts.tmp
 awk '!seen[$0]++' hosts.tmp > hosts
 rm -f hosts.tmp
@@ -34,8 +36,8 @@ sed -i 's/   / /' hosts
 sed -i 's/    / /' hosts
 sed -i 's/     / /' hosts
 sed -i 's/	/ /' hosts
+
 echo "-----hosts file is compressed."
-sleep 1s
 sed -i '/127.0.0.1 localhost/d' hosts
 sed -i '/::1 ip6-localhost ip6-loopback/d' hosts
 sed -i '/fe00::0 ip6-localnet/d' hosts
@@ -48,20 +50,16 @@ echo "#This hosts file is created with LinuxHostsGenerator\n#https://github.com/
 echo "-----hosts file is generated. ✓"
 
 read -p "Do you want to update your hosts file? (y/n)" CONT
-if [ "$CONT" = "y" ]; then
-sudo mv /etc/hosts.bak /etc/hosts.bak.bak
-sudo mv /etc/hosts /etc/hosts.bak
-sudo cp hosts /etc/hosts
-echo "-----hosts file is updated. ✓"
-else
-exit
+if [[ "$CONT" = "y" ]]; then
+    sudo mv /etc/hosts.bak /etc/hosts.bak.bak
+    sudo mv /etc/hosts /etc/hosts.bak
+    sudo cp hosts /etc/hosts
+    echo "-----hosts file is updated. ✓"
 fi
 
 read -p "Do you want to clear your dns cache? (y/n)" CONT
-if [ "$CONT" = "y" ]; then
-sudo /etc/init.d/nscd restart
-sudo systemd-resolve --flush-caches
-echo "-----Your DNS cache is cleared. ✓"
-else
-exit
+if [[ "$CONT" = "y" ]]; then
+    sudo /etc/init.d/nscd restart
+    sudo systemd-resolve --flush-caches
+    echo "-----Your DNS cache is cleared. ✓"
 fi
